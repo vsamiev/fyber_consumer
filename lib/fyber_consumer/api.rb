a447a38b40606755f12040241020c37980ab52b2
Vasily S <zencodycom@gmail.com>
2015-05-24 10:13:27 +0300

    Add api call to service

--- lib/fyber_consumer/api.rb
+++ lib/fyber_consumer/api.rb
@@ -1,4 +1,49 @@
 module FyberConsumer
   class Api
+    # RestClient.log = "stdout"
+
+    PROVIDER_URI = APP_CONFIG['provider_uri']
+    ACCEPT_FORMAT = :json
+
+    def initialize uid, pub0 = nil, page = nil , api_key, params
+      # raise ArgumentError, 'Argument is not numeric' if uid.nil? or
+      @api_key=api_key
+
+      @request_params = Hash.new
+      @request_params['uid'] = uid if uid.to_s.length > 0
+      @request_params['pub0'] = pub0 if pub0.to_s.length > 0
+      @request_params['page'] = page if page.to_s.length > 0
+      @request_params = @request_params.merge params
+    end
+
+    def request
+      signature = FyberConsumer::Signature.new @request_params, @api_key
+      signed_params = {'hashkey' => signature.hashkey}.merge @request_params
+      response = get signed_params
+      request_result = Hash.new
+      case response.code
+        when 200
+          request_result[:status] = "ERR_OK"
+          request_result[:body] = response.body
+        when 400
+          request_result[:status] = "ERR_BAD_REQUEST"
+        when 401
+          request_result[:status] = "ERR_UNAUTHORIZED"
+        when 404
+          request_result[:status] = "ERR_NOT_FOUND"
+        when 500
+          request_result[:status] = "ERR_INTERNAL_ERROR"
+        when 502
+          request_result[:status] = "ERR_BAD_GATEWAY"
+        else
+          request_result[:status] = "ERR_UNKNOWN"
+      end
+      request_result
+    end
+
+    private
+    def get params
+      return RestClient.get PROVIDER_URI, {:accept => ACCEPT_FORMAT, :params => params}
+    end
   end
 end
--- spec/lib/fyber_consumer/api_spec.rb
+++ spec/lib/fyber_consumer/api_spec.rb
@@ -1,8 +1,34 @@
 require 'rails_helper'
-require 'fyber_consumer/api'
+require 'fyber_consumer'
 
 RSpec.describe "Signature" do
   describe "methods" do
+    before(:each) do
+      @params = Hash.new
+      @params['format'] = "json"
+      @params['appid'] = "157"
+      @params['locale'] = "de"
+      @params['os_version'] = "6.0"
+      @params["timestamp"] = Time.now.to_i.to_s
+      # @params["apple_idfa"] = '2E7CE4B3-F68A-44D9-A923-F4E48D92B31E'
+      @params["apple_idfa_tracking_enabled"] = false
+      @params["ip"] = "109.235.143.113"
+      @params["offer_types"] = "112"
+      @params["ps_time"] = "1312211903"
+      @params["device"] = "tablet"
+      @params["device_id"] = "2b6f0cc904d137be2e1730235f5664094b83"
+      @api_key = "b07a12df7d52e6c118e5d47d3f9e60135b109a1f"
+
+      @uid   =  "player1"
+      @pub0  =  "campaign2"
+      @page  =  "2"
+    end
+
+    it 'gets successful response' do
+      api = FyberConsumer::Api.new(@uid, @pub0, @page, @api_key, @params)
+      response = api.request
+      expect(response[:status]).to eq "ERR_OK"
+    end
 
   end
 end
